e05dc868c0d178acc5d107c4f9a86f38ee4f6a40
Pratyaksh7 <pratyakshgupta7@gmail.com>
2021-05-22 12:18:14 +0530

    Recursive + Memoized (Bottom up)

--- Longest Common Subsequence/Problem1.py
+++ Longest Common Subsequence/Problem1.py
@@ -4,17 +4,22 @@ def lcs(X, Y, n, m):
     if n== 0 or m== 0:
         return 0
 
+    if t[n][m] != -1:
+        return t[n][m]
+
     else:
         if X[n-1] == Y[m-1]:
-            return (1+lcs(X, Y, n-1, m-1))
+            t[n][m] = (1+lcs(X, Y, n-1, m-1))
+            return t[n][m]
 
         else:
-            return max(lcs(X, Y, n, m-1), lcs(X, Y, n-1, m))
+            t[n][m] =  max(lcs(X, Y, n, m-1), lcs(X, Y, n-1, m))
+            return t[n][m]
 
-X = "abcdef"
+X = "abcdf"
 Y = "abedlf"
 # o/p: 4 -> abdf
 n = len(X)
 m = len(Y)
-
+t = [[-1 for j in range(m+1)] for i in range(n+1)]
 print(lcs(X, Y, n, m))
