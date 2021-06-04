8f5d36ea71872c1aa85aef2313b974e57002e020
Pratyaksh7 <pratyakshgupta7@gmail.com>
2021-06-04 11:22:58 +0530

    Palindrome Partitioning ( Recursive + Memoized)

--- Matrix Chain Multiplication/Problem2.py
+++ Matrix Chain Multiplication/Problem2.py
@@ -10,13 +10,17 @@ def solve(string, i, j):
     if isPalindrome(string[i:j+1]):  # to check if its palindrome
         return 0
 
+    if t[i][j] != -1:
+        return t[i][j]
+
     mn = maxsize
     for k in range(i, j):
         temp_ans = (1+ solve(string,i,k) + solve(string, k+1, j))
 
         mn = min(temp_ans, mn)
 
-    return mn
+    t[i][j] = mn
+    return t[i][j]
 
 string = "snitin"
 # output: 1 because "s" and "nitin" both are palindrome AND ONLY ONE PARTITION IS DONE
@@ -24,4 +28,5 @@ i = 0
 n = len(string)
 j = n -1
 
+t = [[-1 for _ in range(n+1)] for _ in range(n+1)]
 print(solve(string, i, j))
