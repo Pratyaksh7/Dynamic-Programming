a6be2d30fc2d550a02ea9987df3cbcbb14d53d23
Pratyaksh7 <pratyakshgupta7@gmail.com>
2021-06-10 09:54:42 +0530

    Egg Dropping (recursive and bottom up

--- Matrix Chain Multiplication/Problem5.py
+++ Matrix Chain Multiplication/Problem5.py
@@ -6,14 +6,20 @@ def solve(e,f):
         return f
     if f==0 or f==1:
         return f
+
+    if t[e][f] != -1:
+        return t[e][f]
+
     mn = maxsize
     for k in range(1,f):
         temp = 1 + max(solve(e-1,k-1), solve(e, f-k)) # max is used for the worst case
         mn = min(mn, temp) # min used for the minimum attempts
 
-    return mn
+    t[e][f] = mn
+    return t[e][f]
 
 e= 3 # no of eggs
 f= 5 # no of floors
 
+t = [[-1 for j in range(f+1)]for i in range(e+1)]
 print(solve(e,f))
